aa1d5c378dc414ae50869c4da9199bea24af4224
sskender <sskender@users.noreply.github.com>
2020-03-23 21:00:39 +0100

    3

---
+++ sskender/zadatak3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for file in localhost_access_log.*-02-*.txt;
+do
+    echo "datum: $(date -d $(echo $file | awk -F '.' '{print $2}' ) +'%d-%m-%Y')"
+    echo "--------------------------------------------------"
+
+    formatlog=''
+
+    IFS=$'\n'
+    for line in $(cat $file | awk -F '"' '{print $2}' | sort | uniq);
+    do
+        formatlog+="$(grep -c $line $file) : $line\n"
+    done
+
+    echo -e $formatlog | sort -V -r
+done
