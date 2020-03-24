21c0ca2587d73f5be2f09b3b28bccc274d3b1c7e
sskender <sskender@users.noreply.github.com>
2020-03-24 12:26:23 +0100

    varibale names update

--- sskender/lab1/zadatak4.sh
+++ sskender/lab1/zadatak4.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # source and destination folders
-src=$1
-des=$2
+SRC=$1
+DES=$2
 
 IFS=$'\n' # split by new line
-for image in $(find $src -type f -iname "*.jpg");
+for image in $(find $SRC -type f -iname "*.jpg");
 do
     # create folder (if not exist) from modified date
     modlong=$(stat --format %y $image | awk -F ' ' '{print $1}')
     modshort=$(date -d "$modlong" +'%Y-%m')
-    mkdir -p $des/$modshort; mv $image $des/$modshort/
+    mkdir -p $DES/$modshort; mv $image $DES/$modshort/
 done
