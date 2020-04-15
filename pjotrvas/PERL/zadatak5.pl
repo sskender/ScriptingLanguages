7d23940374934399c5a08b613b67494669d4e2a3
vasiljmatea <matea.vasilj@fer.hr>
2020-04-15 20:26:59 +0200

    FIfth task updated

--- pjotrvas/PERL/zadatak5.pl
+++ pjotrvas/PERL/zadatak5.pl
@@ -2,32 +2,31 @@
 
 $noRed=0;
 
-chomp ($faktori=<>);
+chomp($faktori=<>);
+
 @faktori=split /;/, $faktori;
 
-while (defined ($redak = <>)) {
-	@redak=split /;/, $redak;
-	$jmbag=shift @redak;
-	$prezime=shift @redak;
-	$ime=shift @redak;
-	
-		$i=0;
-		$sumRedak=0;
-		foreach (@redak) {
-			$_=~ s/-/0/;
-			$sumRedak+=$faktori[$i]*$_;
-			$i++;
-		}
-	
-	$rangLista[$noRed]=sprintf("%06.2f;%s;%s;%s", $sumRedak, $jmbag, $prezime, $ime);
-	$noRed++;
+while(defined ($redak=<>)){
+    @redak=split /;/, $redak;
+    $jmbag=$redak[0];
+    $ime=$redak[1];
+    $prezime=$redak[2];
+
+    $sumaRedak=0;
+    for(my $i = 3; $i <= $#redak; $i++){
+        $redak[$i]=~ s/-/0/;
+        $sumaRedak+=$faktori[$i-3]*$redak[$i];
+    } 
+    $rang[$noRed]=sprintf("%06.2f;%s;%s;%s", $sumaRedak, $jmbag, $prezime, $ime);
+   $noRed++; 
 }
 
-@sortRang=reverse sort(@rangLista);
-
+@sortRang=reverse sort (@rang);
 $noRed=1;
+
 foreach (@sortRang) {
-	@redak=split /;/, $_;
-	printf("%3d. %-32s:% 6.2f\n", $noRed, "$redak[2], $redak[3] ($redak[1])", $redak[0]);
-	$noRed++;
-}
+    @redak=split /;/, $_;
+    printf("%3d. %-32s:% 6.2f\n", $noRed, "$redak[2], $redak[3] ($redak[1])", $redak[0]);
+     $noRed++;
+ }
+
