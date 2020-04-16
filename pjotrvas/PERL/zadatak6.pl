8bcbe9c86c294b87d042758fcb5dcdb66db8ca05
vasiljmatea <matea.vasilj@fer.hr>
2020-04-16 16:08:33 +0200

    final update

--- pjotrvas/PERL/zadatak6.pl
+++ pjotrvas/PERL/zadatak6.pl
@@ -3,19 +3,19 @@
 $duljina = pop(@ARGV);
 
 while(<>) {
-        chomp;
+    chomp;
 	
-	$tekst=lc $_;	
-	$tekst=~ s/\W/\ /g;
+	$tekst = lc $_;	
+	$tekst =~ s/\W/\ /g;
 
-	@prefiksi= ($tekst=~ m/[a-z]{$duljina}/g);
+	@prefiksi = ($tekst =~ m/[a-z]{$duljina}/g);
 
 	foreach $rijec (@prefiksi){
 		$popis{$rijec} += 1;
 	}
 }
 	
-@sortPopis=(sort keys %popis);
+@sortPopis = (sort keys %popis);
 
 foreach $r (@sortPopis){
 	print "$r : $popis{$r}" . "\n";
