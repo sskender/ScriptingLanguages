c3b062ce7fc0f07298649c3154d9e9f96066e37e
vasiljmatea <matea.vasilj@fer.hr>
2020-04-17 16:39:09 +0200

    new solution

---
+++ pjotrvas/PERL/zadatak3b.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+
+while(<>){
+    push @zapisi, $_;
+}
+
+foreach (@zapisi){
+        @splitRed = split /:/, $_;
+        $datum = &datum();
+        $mjeseci{$datum} += 1;
+    }
+
+    while (my ($k,$v)= each %mjeseci){
+        
+        foreach (0..23){
+		    $sati[$_] = 0;
+	    }
+
+        foreach (@zapisi){
+        @splitRed = split /:/, $_;
+        $datum = &datum();
+            
+            if($datum eq $k){
+                $sat = $splitRed[1];
+                $sati[$sat] += 1; 
+            }
+        }
+        &printer($k);
+    }   
+
+sub printer{
+    my ($datum) = @_;
+	print "Datum: " . $datum . "\n" . "sat : broj pristupa\n";
+	print "-------------------------------\n";		
+
+    $indeks = 0;
+	foreach (@sati) {
+		printf (" %02d : %s", $indeks++ , $_ . "\n");
+         }
+	print "\n";		
+}
+
+sub datum{
+    @dio = split / /, $splitRed[0];
+    $dio[3]=~ s|^.*\[(.{2}/.{3}/.{4}).*$|$1|;
+    return $dio[3];
+}
