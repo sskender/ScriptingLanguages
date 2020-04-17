40d2dec31d8714432f179be8fc74805b98bb1d8c
sskender <sskender@users.noreply.github.com>
2020-04-17 17:28:53 +0200

    update

--- pjotrvas/PERL/zadatak3b.pl
+++ pjotrvas/PERL/zadatak3b.pl
@@ -2,16 +2,12 @@
 
 while(<>){
     push @zapisi, $_;
+    @splitRed = split /:/, $_;
+    $datum = &datum();
+    $datumi{$datum} += 1;
 }
 
-foreach (@zapisi){
-        @splitRed = split /:/, $_;
-        $datum = &datum();
-        $datumi{$datum} += 1;
-    }
-
     while (my ($k,$v)= each %datumi){
-        
         foreach (0..23){
 		    $sati[$_] = 0;
 	    }
@@ -19,25 +15,25 @@ foreach (@zapisi){
         foreach (@zapisi){
         @splitRed = split /:/, $_;
         $datum = &datum();
-            
+
             if($datum eq $k){
                 $sat = $splitRed[1];
-                $sati[$sat] += 1; 
+                $sati[$sat] += 1;
             }
         }
         &printer($k);
-    }   
+    }
 
 sub printer{
     my ($datum) = @_;
 	print "Datum: " . $datum . "\n" . "sat : broj pristupa\n";
-	print "-------------------------------\n";		
+	print "-------------------------------\n";
 
     $indeks = 0;
 	foreach (@sati) {
 		printf (" %02d : %s", $indeks++ , $_ . "\n");
          }
-	print "\n";		
+	print "\n";
 }
 
 sub datum{
