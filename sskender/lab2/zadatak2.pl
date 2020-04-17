6f90fa33ca24ae9f9446dd6df763885c501295e2
sskender <sskender@users.noreply.github.com>
2020-04-17 14:41:16 +0200

    lab2: 2 fix defined

--- sskender/lab2/zadatak2.pl
+++ sskender/lab2/zadatak2.pl
@@ -6,7 +6,7 @@ my $input_number;
 my @all_numbers;
 my $sum = 0;
 
-while (chomp($input_number = <STDIN>) && $input_number ne "") {
+while (defined(chomp($input_number = <STDIN>)) && $input_number ne "") {
     push @all_numbers, $input_number;
     $sum += $input_number;
 }
