d6e2c02d90a82753de04d6c0db519410154df012
sskender <sskender@users.noreply.github.com>
2020-04-17 18:19:46 +0200

    lab2: 4 fix

--- sskender/lab2/zadatak4.pl
+++ sskender/lab2/zadatak4.pl
@@ -21,10 +21,10 @@ while (defined($line = <>)) {
     # extract term values
     my @term = split / /, $students[3];
     my $term_date = $term[0];
-    my $term_time_end = $term[2];
+    my $term_time_start = $term[1];
 
     # convert to seconds
-    my @term_end_array = split /:/, $term_time_end;
+    my @term_end_array = split /:/, $term_time_start;
     my $term_deadline_seconds = ($term_end_array[0] * 60 * 60 + $term_end_array[1] * 60);
 
     # extract locked values
@@ -36,7 +36,7 @@ while (defined($line = <>)) {
     my @locked_time_array = split /:/, $locked_time;
     my $locked_seconds = ($locked_time_array[0] * 60 * 60 + $locked_time_array[1] * 60 + $locked_time_array[2]);
 
-    if ($term_date ne $locked_date || $locked_seconds >= $term_deadline_seconds) {
-        print "$jmbag $last_name $first_name - PROBLEM: $term_date $term_time_end --> $locked_date $locked_time\n";
+    if ($term_date ne $locked_date || $locked_seconds - $term_deadline_seconds > 60 * 60) {
+        print "$jmbag $last_name $first_name - PROBLEM: $term_date $term_time_start --> $locked_date $locked_time\n";
     }
 }
