79da0751ea81cb9b962e42c5d3a8b250a2ea837b
vasiljmatea <matea.vasilj@fer.hr>
2020-05-16 14:28:39 +0200

    final version

--- pjotrvas/PYTHON/zadatak3.py
+++ pjotrvas/PYTHON/zadatak3.py
@@ -1,22 +1,12 @@
-import os
-import re
-import sys
+import os, re, sys
 
 datoteke = os.listdir(".")
+
 ocjene = {}
 student = []
 vjezbe = []
 
-
 for i in range(len(datoteke)):
-    if re.match('studenti*', datoteke[i]):
-        studentiDat = open(datoteke[i], 'r')
-        studenti = studentiDat.readlines()
-        for i in range(len(studenti)):
-            student.append(studenti[i].split(' '))
-
-for i in range(len(datoteke)):
-
     if re.match('Lab*', datoteke[i]):
         vjezba = datoteke[i].split('_')[1:-1]
         vjezba = int(vjezba[0])
@@ -29,22 +19,27 @@ for i in range(len(datoteke)):
             jmbag = zapisi[0].split(' ')[0]
             ocjena = zapisi[0].split(' ')[1]
             ocjena = float(ocjena)
-            #if (vjezba, jmbag) in ocjene:
-                #exit("Vec postoji zapis o studentu {} u vjezbi {}!".format(jmbag, vjezba))
-            ocjene[(vjezba, jmbag)] = (ocjena)
-
+            if (vjezba, jmbag) in ocjene:
+                exit("Vec postoji zapis o studentu {} u vjezbi {}!".format(jmbag, vjezba))
+            ocjene[(vjezba, jmbag)] = ocjena
+    elif re.match('studenti*', datoteke[i]):
+        studentiDat = open(datoteke[i], 'r')
+        studenti = studentiDat.readlines()
+        for i in range(len(studenti)):
+            student.append(studenti[i].split(' '))
 
 print("JMBAG\t\tPrezime, Ime\t", end='')
+vjezbe.sort()
 for vjezba in vjezbe:
     print("L%d\t"%vjezba, end='')
 print()
 
-for student in student:
-    print("{}\t{},{}".format(student[0], student[1], student[2].strip()), end='\t')
-    for ocjena in sorted(ocjene):
-        if ocjena[1] == student[0]:
-            print("%.1f"%ocjene[ocjena], end='\t')
+for studentJedan in student:
+    print("{}\t{}, {}".format(studentJedan[0], studentJedan[1], studentJedan[2].strip()), end='\t')
+    for vjezba in vjezbe:
+        kljuc = (vjezba, studentJedan[0])
+        if kljuc in ocjene:
+            print("%.1f"%ocjene[kljuc], end='\t')
+        else:
+            print ("0.0", end='\t')
     print()
-#print(student)
-#print(ocjene)
-#print(vjezbe)
