e07633308cc62edb0bedc078a19c395a7fe6a670
sskender <sskender@users.noreply.github.com>
2020-05-16 13:53:09 +0200

    Changes

--- pjotrvas/PYTHON/zadatak3.py
+++ pjotrvas/PYTHON/zadatak3.py
@@ -5,11 +5,23 @@ import sys
 datoteke = os.listdir(".")
 ocjene = {}
 student = []
+vjezbe = []
+
 
 for i in range(len(datoteke)):
+    if re.match('studenti*', datoteke[i]):
+        studentiDat = open(datoteke[i], 'r')
+        studenti = studentiDat.readlines()
+        for i in range(len(studenti)):
+            student.append(studenti[i].split(' '))
+
+for i in range(len(datoteke)):
+
     if re.match('Lab*', datoteke[i]):
         vjezba = datoteke[i].split('_')[1:-1]
-        vjezba = float(vjezba[0])
+        vjezba = int(vjezba[0])
+        if vjezba not in vjezbe:
+            vjezbe.append(vjezba)
         dat = open(datoteke[i], 'r')
         sadrzaj = dat.readlines()
         for zapis in sadrzaj:
@@ -17,9 +29,22 @@ for i in range(len(datoteke)):
             jmbag = zapisi[0].split(' ')[0]
             ocjena = zapisi[0].split(' ')[1]
             ocjena = float(ocjena)
-            ocjene[jmbag] = (ocjena, vjezba)            
-    elif re.match('studenti*', datoteke[i]):
-        studentiDat = open(datoteke[i], 'r')
-        studenti = studentiDat.readlines()
-        for i in range(len(studenti)):
-            student.append(studenti[i].split(' '))
+            #if (vjezba, jmbag) in ocjene:
+                #exit("Vec postoji zapis o studentu {} u vjezbi {}!".format(jmbag, vjezba))
+            ocjene[(vjezba, jmbag)] = (ocjena)
+
+
+print("JMBAG\t\tPrezime, Ime\t", end='')
+for vjezba in vjezbe:
+    print("L%d\t"%vjezba, end='')
+print()
+
+for student in student:
+    print("{}\t{},{}".format(student[0], student[1], student[2].strip()), end='\t')
+    for ocjena in sorted(ocjene):
+        if ocjena[1] == student[0]:
+            print("%.1f"%ocjene[ocjena], end='\t')
+    print()
+#print(student)
+#print(ocjene)
+#print(vjezbe)
