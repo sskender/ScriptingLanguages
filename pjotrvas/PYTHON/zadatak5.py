bfca6051f9e99b03e79ef309fd7f61b1c22daf12
vasiljmatea <matea.vasilj@fer.hr>
2020-05-21 11:52:57 +0200

    3. lab izlazni test

---
+++ pjotrvas/PYTHON/zadatak5.py
@@ -0,0 +1,32 @@
+import sys, re
+
+JEDAN = 1
+
+if len(sys.argv) > JEDAN:
+    html_files = sys.argv[1:]
+else:
+    exit("POGRESKA: Nedovoljan broj argumenata.")
+
+data_hash = {}
+
+for html_file in html_files:
+    html_open = open(html_file, "r")
+    html_data = html_open.readlines()
+    html_open.close()
+
+    for data in html_data:
+        data_tag = re.findall("<[\w]+", data)
+        if len(data_tag) > 0:
+            for tag in data_tag:
+                tag = tag.replace("<", "")
+                tag = tag.lower()
+                if tag in data_hash:
+                    data_hash[tag] = data_hash[tag] + 1
+                else:
+                    data_hash[tag] = 1
+
+print("Statistika tagova:\nTag               Broj ponavljanja\n==================================")
+
+for data in data_hash:
+    print("{:<10s} {:>22d}".format(data, data_hash[data]))
+
